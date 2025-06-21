3ea09cc469d753a8405e5cf7738f802afc9af24a
VishalSaini7272 <sainivishal156@gmail.com>
2025-06-21 19:05:52 +0530

    code add

--- day2.js
+++ day2.js
@@ -1,2 +1,105 @@
-let now =  new date(02/03/2025);
-console.log(now);
+/*let r =4;
+const pi=3;
+console.log(pi*r*r);
+console.log(2*pi*r);*/
+
+/*let a=8;
+let b=3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);*/
+// let C=25;
+  // console.log(C*(9/5))+32;
+  // let example = "HELLO VISHAL";
+  // console.log(example.toLowerCase());
+
+
+  let example = "HELLO EVERYONE";
+  // console.log(example.toLowerCase());
+
+  let newName = "Hello everyone";
+  let newMyname = newName.indexOf("ll")
+  // console.log(newMyname);
+
+  let string = "hello world".replace("hello","hii");
+  // console.log(string);
+  // let oneArr = [1,2,3,4,5]
+  // console.log(oneArr[2]);
+
+  //  let oneArr = [1,2,3,4,5]
+  //  oneArr.includes(7);
+  //  console.log(oneArr)
+
+  // const myArr = ["1","2","3","4","5","6"]
+  // myArr.join();
+  // console.log(typeof myArr);
+
+// const Array = [1,2,3,4,5,6]
+  // const myn1 =  myArr.splice(0,2)
+  // console.log(Array.from("vishal"));
+// console.log(typeof "vishal")
+  let a =10
+  b= 20
+  // console.log(Array.of(a,b));
+
+// let n = 6;
+//   for(let i=1; i<=n; i++){
+//     let str = "";
+//     for(let j=1; j<=i; j++){
+// str = str + "* ";
+//     }
+//     console.log(str);
+//   }
+
+
+
+
+
+
+
+
+
+  let n = 5;
+for(let i=1; i<=n; i++){
+    let str = "";
+for(let j=1; j<=i; j++){
+str = str + "* ";
+}
+// console.log(str);
+}
+
+// // let m = n-1;
+// for(let i=m; i>=1; i--){
+//   let str="";
+//  for(let j=1; j<=i; j++){
+// str = str + "* ";
+//  }
+// //  console.log(str);
+// }
+
+
+
+
+
+
+
+// let n = 3432123;
+// for(let i=1; i<=n; i++){
+ 
+// }
+// console.log(n);
+
+
+// let num = 3432123;
+// while(num>0){
+//   temp= num%10
+//   console.log(temp);
+//   num = Math.floor(num/10);
+// // }
+
+// let num = 8;
+// for(let i=1; i<=10; i++){
+// console.log(`${num} x ${i} = ${num*i}` );
+// }
